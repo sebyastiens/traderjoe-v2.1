ffb7f9a415ca68d84c321972c851346a6899d11b
sebyastiens <sebyastiens@gmail.com>
2024-02-28 05:30:03 +0100

    Update ILBRouter.sol

--- src/interfaces/ILBRouter.sol
+++ src/interfaces/ILBRouter.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.10;
 
-import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
-
+//import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol"; // ORIGINAL
+import {IERC20} from "./IERC20.sol"; // MODIFIED PATH
 import {IJoeFactory} from "./IJoeFactory.sol";
 import {ILBFactory} from "./ILBFactory.sol";
 import {ILBLegacyFactory} from "./ILBLegacyFactory.sol";
