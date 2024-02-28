0a06328e945753916764255392bc583b127003cf
sebyastiens <sebyastiens@gmail.com>
2024-02-28 05:28:54 +0100

    Update ILBFactory.sol

--- src/interfaces/ILBFactory.sol
+++ src/interfaces/ILBFactory.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.10;
 
-import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
-
+//import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol"; // ORIGINAL
+import {IERC20} from "./IERC20.sol"; // MODIFIED PATH
 import {ILBPair} from "./ILBPair.sol";
 import {IPendingOwnable} from "./IPendingOwnable.sol";
 
